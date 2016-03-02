cdc18f84736184b7e67c4fb384fd198f305abfa0
Michael Weems <mweems5@gmail.com>
2016-03-02 17:29:29 -0600

    Prettied up the interface, outputs the requests directly to the page now - not in console.

--- Lab6/Library/booksLibrary.js
+++ Lab6/Library/booksLibrary.js
@@ -16,22 +16,65 @@ function Library(){
 		for (i = 0; i < this.shelfcount; i++) {
 			for (j = 0; j < this.shelves[i].bookcount; j++) {
 				if ( this.shelves[i].books[j].name == name ) {
-					console.log( "The book " + name + " is available on " + this.shelves[i].name );
-					return;
+					//console.log( "The book " + name + " is available on " + this.shelves[i].name );
+					return "The book " + name + " is available on " + this.shelves[i].name;
 				}
 			}
 		}
-		console.log("The book is not available");
+		//console.log("The book, " + name + ", is not available");
+		return "The book, " + name + ", is not available";
 	};
-	// returns how many of this book are in the library
-	this.getDetails = function(name) {
-		total = 0;
-		
-		for (i = 0; i < this.shelfcount; i++) {
-			total += this.shelves[i].bookcountArr[name];
+	
+	
+	// ---------------------------------------------------------------------------
+	// Create the Table content
+
+	this.showLibrary = function() {
+		mytable = $("<table border='2'></table>"); // creates DOM elements
+		mytablebody = $('<tbody></tbody>'); 
+
+		curr_row = $('<tr style="background-color:green"></tr>');
+		for(col = 0; col < 3; col++) {
+			curr_cell = $('<td></td>');
+			curr_text = this.shelves[col].name;
+			curr_cell.append(curr_text);
+			curr_row.append(curr_cell);
 		}
-		console.log("The Library has " + total + " copies of " + name); 
-	};
+		mytablebody.append(curr_row); // appends arg to mytablebody
+
+		for(row = 0; row < 11; row++) {
+			curr_row = $('<tr></tr>');
+			
+			for(col = 0; col < 3; col++) {
+				curr_cell = $('<td style="width:126px"></td>');
+				curr_text = '';
+				if ( this.shelves[col].books[row] != undefined ) {
+					curr_text = this.shelves[col].books[row].name;
+				}
+				curr_cell.append(curr_text);
+				curr_row.append(curr_cell);
+			  }
+			  mytablebody.append(curr_row); // appends arg to mytablebody
+		}
+		mytable.append(mytablebody);
+		mytable.insertBefore($('#tableEnd')); // real dom from document!
+		
+		sCount = this.shelfcount;
+		sArr = this.shelves;
+		
+		$("td").click( function() {
+			name = $(this).text();
+			total = 0;
+			for (i = 0; i < sCount; i++) {
+				total += sArr[i].bookcountArr[name];
+			}
+			console.log("The Library has " + total + " copies of " + name); 
+		});
+	}
+
+	this.hideLibrary = function() {
+		$("table").remove();
+	}
 };
 
 // -------------
@@ -56,7 +99,6 @@ function Book(bookName) {
 
 var lib = new Library();
 
-var shelfArr = {};
 for (var i = 0; i < 3; i++) {
 	s = new Shelf();
 	s.name = "Shelf " + i;
@@ -80,70 +122,51 @@ for (i = 0; i < 3; i++) {
 $(document).ready(function() {
     $("#isAvailable").click( function() {
 		text = $("#isAvailText").val();
-		lib.isBookAvailable(text);
+		response = lib.isBookAvailable(text);
+		$("#responseText").val(response);
+		$("#isAvailText").val("");
     });
 	
-	$("#listShelves").click( function() {
-		for ( i = 0; i < lib.shelfcount; i++ ) {
-			console.log(lib.shelves[i]);
+	$("#isAvailText").bind("isAvail", function(e){
+		text = $("#isAvailText").val();
+		response = lib.isBookAvailable(text);
+		console.log(response);
+		$("#responseText").val(response);
+		$("#isAvailText").val("");
+	});
+	$("#isAvailText").keyup(function(e){
+		if(e.keyCode == 13)
+		{
+			$(this).trigger("isAvail");
 		}
+	});
+	
+	$("#listShelves").click( function() {
+		response = "";
+		for ( i = 0; i < lib.shelfcount; i++ ) {
+			//console.log(lib.shelves[i]);
+			response += lib.shelves[i].name;
+			response += "\t Book count: " + lib.shelves[i].bookcount + "\n";
+			for (j = 0; j < lib.shelves[i].bookcount; j++) {
+				response += "\t - " + lib.shelves[i].books[j].name + "\n";
+			}
+			response += "\n";
+		}
+		$("#responseText").val(response);
     });
 	
 	$("#showLibrary").click( function() {
 		if (!lib.showLib) {
 			lib.showLib = true;
-			showLibrary();
+			lib.showLibrary();
 			$("#showLibrary").attr("value","Hide the Library!");
 		}
 		else {
 			lib.showLib = false;
-			hideLibrary();
+			lib.hideLibrary();
 			$("#showLibrary").attr("value","Show the Library!");
 		}
-		
     });
-	
 }); // end of ready
 
 
-// ---------------------------------------------------------------------------
-// Create the Table content
-
-function showLibrary() {
-	mytable = $("<table border='2'></table>"); // creates DOM elements
-	mytablebody = $('<tbody></tbody>'); 
-
-	curr_row = $('<tr style="background-color:green"></tr>');
-	for(col = 0; col < 3; col++) {
-		curr_cell = $('<td></td>');
-		curr_text = lib.getShelf(col).name;
-		curr_cell.append(curr_text);
-		curr_row.append(curr_cell);
-	}
-	mytablebody.append(curr_row); // appends arg to mytablebody
-
-	for(row = 0; row < 11; row++) {
-		curr_row = $('<tr></tr>');
-		
-		for(col = 0; col < 3; col++) {
-			curr_cell = $('<td></td>');
-			curr_text = '';
-			if ( lib.getShelf(col).books[row] != undefined ) {
-				curr_text = lib.getShelf(col).books[row].name;
-			}
-			curr_cell.append(curr_text);
-			curr_row.append(curr_cell);
-		  }
-		  mytablebody.append(curr_row); // appends arg to mytablebody
-	}
-	mytable.append(mytablebody);
-	mytable.insertBefore($('#tableEnd')); // real dom from document!
-	
-	$("td").click( function() {
-		lib.getDetails($(this).text());
-    });
-}
-
-function hideLibrary() {
-	$("table").remove();
-}
