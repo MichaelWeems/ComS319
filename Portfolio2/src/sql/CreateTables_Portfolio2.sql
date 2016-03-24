093141c6120f68f40abab761707d77eccc291c11
Michael Weems <mweems5@gmail.com>
2016-03-23 20:45:24 -0500

    SQL - new tables / deleted unnecessary tables

--- Portfolio2/src/sql/CreateTables_Portfolio2.sql
+++ Portfolio2/src/sql/CreateTables_Portfolio2.sql
@@ -14,25 +14,41 @@ values('ztwild', '48fa4c38043122c03a61b1fb03d378ee', 'false'),
 ('admin', '0cc175b9c0f1b6a831c399e269772661', 'true');
 
 
-create table if not exists Group8_userposts(
-postId int(4) not null AUTO_INCREMENT,
-username varchar(10) not null,
-primary key(postId),
-foreign key(username) references Group8_users(username)
-);
+create table if not exists Group8_friends(
+username varchar(10),
+friend varchar(10),
+foreign key(username) references Group8_users(username));
 
-insert into Group8_userposts(username)
-values('mdweems'),('user'),('ztwild');
+insert into Group8_friends(username, friend)
+values('mdweems', 'ztwild'),
+('ztwild', 'mdweems'),
+('mdweems', 'jazz');
+
 
 create table if not exists Group8_posts(
 title varchar(50),
 text varchar(4000),
-data varchar(250),
-postId int(4) not null,
-foreign key(postId) references Group8_userposts(postId));
+data varchar(250),    
+postId int(4) not null AUTO_INCREMENT,
+username varchar(10),
+primary key(postId),
+foreign key(username) references Group8_users(username));
 
-insert into Group8_posts(postId, title)
-values(1, 'Jazz'),(2, 'Uh Huh...'),(3, 'Yezzir');
+insert into Group8_posts(title, text, username)
+values('Jazz', 'McGee', 'mdweems'),('wooooo hoooooooo', 'Uh Huh...', 'mdweems'),('juicy', 'Yezzir', 'mdweems');
+
+
+create table if not exists Group8_comments(
+commentId int not null AUTO_INCREMENT,
+text varchar(250),
+username varchar(10) not null,
+postId int(4) not null,
+primary key(commentId),
+foreign key(postId) references Group8_posts(postId),
+foreign key(username) references Group8_users(username));
+
+insert into Group8_comments(username, postId, text)
+values('mdweems', 1, 'jazzity jazzin jazz'),('mdweems', 2 ,  'Uh Huh...'),('juicy',3, 'Yezzir');
 
 
 create table if not exists Group8_apps(
@@ -44,14 +60,3 @@ primary key(name)
 
 insert into Group8_apps(name, location)
 values('SightReader', 'src\\apps\\SightReader');
-
-
-create table if not exists Group8_friends(
-username varchar(10),
-friend varchar(10),
-foreign key(username) references Group8_users(username));
-
-insert into Group8_friends(username, friend)
-values('mdweems', 'ztwild'),
-('ztwild', 'mdweems'),
-('mdweems', 'jazz');
--- Portfolio2/src/sql/DropTables_Portfolio2.sql
+++ Portfolio2/src/sql/DropTables_Portfolio2.sql
@@ -4,8 +4,10 @@ drop table Group8_users;
 
 drop table Group8_apps;
 
-drop table Group8_userposts;
-
 drop table Group8_posts;
+
+drop table Group8_comments;
+
+drop table Group8_friends;
 
 set FOREIGN_KEY_CHECKS=1;
