46b59470f52f39a8139994440fdf519b78dfaf56
ztwild <ztwild@iastate.edu>
2016-04-15 20:59:35 -0500

    Randomized Library Shelfs

--- Lab11/app.js
+++ Lab11/app.js
@@ -1,70 +1,73 @@
-//
-// Allows js to be in its own file and
-// html to be in its own file
-//
-var app = angular.module('myApp', ['ngRoute']);
 
-app.config(['$routeProvider', function ($routeProvider,$locationProvider) {
-  $routeProvider
-    .when("/", {templateUrl: "template_library.html", controller: "LibraryControl"})
-    .when("/Book", {templateUrl: "template_book.html", controller: "BookControl"})
-    .otherwise({redirectTo: "/"});
-}]);
+var app = angular.module("libApp", ['ngRoute'])
 
-app.controller('LibraryControl', function($scope, $location, selectedbook) {
-    
-  $scope.shelves = [{1:"Book1",2:"Book3",3:"Book6",4:"Book5",5:"Book9",6:"Book7",7:"Book2"},
-                    {1:"Book4",2:"Book5",3:"Book2",4:"Book6",5:"Book4",6:"Book8",7:"Book6"},
-                    {1:"Book3",2:"Book5",3:"Book7",4:"Book4",5:"Book2",6:"Book7",7:"Book3"}];
-    
-  var library = [];
+.config(["$routeProvider", function($routeProvider,$locationProvider){
+    $routeProvider.when('/library', {templateUrl:"template_library.html", controller:"libController"})
+    .when('/book', {templateUrl:"template_book.html", controller:"bookController"})
+    .otherwise({redirectTo:'/library'});
+}])
 
-  /* Convert Shelves to rows */
-  count = 0;
-  for (var ele in $scope.shelves[0]){
-      count++;
-  }
-
-  for (i=1;i<=count;i++){
-      var row = {};
-      for (j=0; j < $scope.shelves.length; j++){
-          row[j+1] = $scope.shelves[j][i];
-      }
-      library.push(row);
-  }
-  /****************************/
-    
-  $scope.getLibrary = function(){
-      return library;
-  }
-  
-  $scope.bookdetails = function(addr, name){
-      selectedbook.setName(name);
-      $location.path(addr);
-  }
-  
-});
-
-app.controller('BookControl', function($scope, $location, selectedbook) {
-  $scope.name = selectedbook.getName();
-  $scope.checkedout = "Not Checked out";
-  $scope.author = "author";
-    
-  $scope.seeLibrary = function(addr){
-      selectedbook.setName("");
-      $location.path(addr);
-  }
-});
-
-app.factory('selectedbook', function(){
-    var book = {};
-    var name = "";
-    book.setName = function(str){
-        name = str;
+.controller("startController", function($scope, $location){
+    var lib = new Library();
+    lib.fillShelves();
+    $scope.shelves = [];
+    $scope.book = new Book();
+    for(i = 0; i < 10; i++){
+        $scope.shelves.push({shelf1:lib.getBooks(0)[i], shelf2:lib.getBooks(1)[i], shelf3:lib.getBooks(2)[i]})
     }
-    book.getName = function(){
-        return name;
+})
+
+.controller("libController", function($scope, $location){  
+    $scope.load = function(novel){
+        $scope.book = novel;
+        console.log($scope.book.name+" : "+$scope.book.copies+" Copies lib controller");
+        $location.path("/book");
     }
-    return book;
+})
+
+.controller("bookController", function($scope, $location){
+//    console.log($scope.book.name+" : "+$scope.book.copies+" Copies book controller");
+    $scope.back = function(){
+        $location.path("/library");
+    }
 });
 
+//Library
+function Library(){
+    this.name = "Library";
+    this.shelves = [new Shelf(), new Shelf(), new Shelf()];
+    this.books = [];
+    
+    this.getBooks = function(shelf){
+      return this.shelves[shelf].books;  
+    };
+		
+    this.fillShelves = function(){
+		for(i = 0; i < 10; i++){
+            this.books.push(new Book());
+        }
+        for(j = 0; j < 3; j++){
+            for(k = 0; k < 10; k++){
+                var num = Math.floor(Math.random()* 10);
+                this.shelves[j].books.push(this.books[num]);
+                this.books[num].rtrn();
+            };
+        };
+    };
+};
+//Shelf
+var s = 0;
+function Shelf(){
+    ++s;
+	this.name = "Shelf "+s; 
+    this.books = [];
+	};
+//Book
+var b = 0;
+function Book(){
+	this.copies = 0;
+	this.name = "Book "+ (++b);
+    
+    this.rent = function() {--this.copies};
+    this.rtrn = function() {++this.copies};
+};
